e3b92d3d26cb324ab69b23fc3c96e7490df1b2b1
Varnita Jain <varnita.jain@hotwax.co>
2025-11-17 09:57:37 +0530

    Improved:transfer orderflow updated

--- tests/fullfillment-app/transfer-order-flow/test-discardorder.spec.js
+++ tests/fullfillment-app/transfer-order-flow/test-discardorder.spec.js
@@ -1,37 +1,16 @@
-import { test, expect } from "@playwright/test";
+import { test } from "@playwright/test";
+
+import TransferOrderFlowPage from "../../pages/fullfillment-app/transfer-orders/transfer-order-flow.page";
 
 test("test", async ({ page }) => {
-  await page.waitForTimeout(5000);
-  await page.goto("https://fulfillment-dev.hotwax.io/open");
-  await page.getByText("Transfer Orders").click();
+  const transferOrderFlow = new TransferOrderFlowPage(page);
 
-  await page.waitForTimeout(5000);
-  await page.getByTestId("create-transfer-order-btn").locator("path").nth(1).click();
-  // await page.getByRole("textbox", { name: "Transfer name" }).click();
-  await page
-    .getByRole("textbox", { name: "Transfer name" })
-    .fill("Discard Order Test 01");
-  await page.waitForTimeout(3000);
-  await page.getByRole("searchbox", { name: "search text" }).click();
-  await page.getByRole("searchbox", { name: "search text" }).fill("central");
-  await page.getByTestId("facility-radio-options").click();
-  await page.getByTestId("save-transfer-order-btn").click();
-  await page.waitForTimeout(3000);
-
-  // After saving order next steps
-
-  // selecting serach tab
-  await page.locator("ion-segment-button").nth(1).click();
-
-  // Adding item to Transfer Order
-  await page
-    .getByTestId("search-product-input")
-    .getByRole("searchbox", { name: "search text" })
-    .fill("MH09");
-  await page.getByRole("button", { name: "Add to Transfer" }).click();
-  await page.waitForTimeout(5000);
-
-  // Discard order
-  await page.getByRole("button", { name: "Discard order" }).click();
-  await page.waitForTimeout(3000);
+  await transferOrderFlow.navigateToTransferOrders();
+  await transferOrderFlow.createTransferOrder(
+    "Discard Order Test 01",
+    "central"
+  );
+  await transferOrderFlow.openSearchTab();
+  await transferOrderFlow.searchAndAddProduct("MH09");
+  await transferOrderFlow.discardOrder();
 });
