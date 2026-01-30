405f0a0fdb9cd42292695e6b1533b5dfbab1247e
Varnita Jain <varnita.jain@hotwax.co>
2026-01-30 15:43:41 +0530

    job manager flow

---
+++ tests/jobmanager-app/jobmanager-pipe-runnow.spec.js
@@ -0,0 +1,51 @@
+import { test, expect } from "@playwright/test";
+
+test("Sanity | Fulfillment order flow - Pick, Pack and Ship", async ({
+  page,
+}) => {
+  // Step 1: Navigate to Job Manager pipeline page
+  await page.goto("https://job-manager-dev.hotwax.io/pipeline");
+  await page.waitForTimeout(2000);
+
+  await page.locator("#ion-input-0").fill("hotwax.user");
+  await page.locator("#ion-input-1").fill("hotwax@786");
+  await page.waitForTimeout(1000);
+  await page.getByRole("button", { name: "Login" }).click();
+  await page.waitForTimeout(1000);
+
+  // Assertion: Ensure pipeline page is loaded.
+  await expect(
+    page.locator("ion-title", { hasText: "Job Manager" }),
+  ).toBeVisible();
+
+  // Ensure the pipeline tab is visible and clicked
+  await page.locator("ion-item", { hasText: "Pipeline" }).click();
+
+  await page.waitForTimeout(2000);
+
+  // refresh the page to load jobs latest data
+  await page.reload();
+  await page.waitForTimeout(2000);
+  // click on the 1st job at the pipeline to run now
+  await page.locator("ion-card").first().click();
+  await page.waitForTimeout(2000);
+  await page.getByRole("button", { name: "Run now", exact: true }).click();
+  await page.waitForTimeout(2000);
+
+  // Click on Save button inside modal
+  await page.getByRole("button", { name: "Save", exact: true }).click();
+  await page.waitForTimeout(2000);
+  // Wait for dialog to appear
+  await page.locator(".alert-button-group").waitFor({ state: "visible" });
+
+  // Click Run now inside dialog
+  await page.getByRole("button", { name: "Run now", exact: true }).click();
+  await page.waitForTimeout(2000);
+  await page.locator("ion-segment-button", { hasText: "History" }).click();
+  await page.waitForTimeout(2000);
+  // Verify that the job is moved to History tab and cancelled status is visible.
+  await expect(
+    page.locator(".job-card, ion-card").first().locator("text=Finished"),
+  ).toBeVisible();
+  await page.waitForTimeout(2000);
+});
